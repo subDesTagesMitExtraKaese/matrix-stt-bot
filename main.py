abfff93779f2538d0d620b7095fd73c469ea737e
subDesTagesMitExtraKaese <jmueller@justprojects.de>
2026-08-08 21:54:55 +0200

    format documents

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from urllib.parse import urlparse
 import os
-import time
 import traceback
 import tempfile
 import asyncio
@@ -13,94 +12,98 @@ import nio
 from speech_recognition import ASR
 
 creds = botlib.Creds(
-  homeserver=os.environ['HOMESERVER'],
-  username=os.environ['USERNAME'],
-  password=os.getenv('PASSWORD', None),
-  login_token=os.getenv('LOGIN_TOKEN', None),
-  access_token=os.getenv('ACCESS_TOKEN', None),
-  session_stored_file="/data/session.txt"
+    homeserver=os.environ["HOMESERVER"],
+    username=os.environ["USERNAME"],
+    password=os.getenv("PASSWORD", None),
+    login_token=os.getenv("LOGIN_TOKEN", None),
+    access_token=os.getenv("ACCESS_TOKEN", None),
+    session_stored_file="/data/session.txt",
 )
 
 config = botlib.Config()
 config.encryption_enabled = True
 config.emoji_verify = False
 config.ignore_unverified_devices = True
-config.store_path = '/data/crypto_store/'
-if 'ALLOWLIST' in os.environ:
-  config.allowlist = os.environ['ALLOWLIST'].split(',')
+config.store_path = "/data/crypto_store/"
+if "ALLOWLIST" in os.environ:
+    config.allowlist = os.environ["ALLOWLIST"].split(",")
 
 bot = botlib.Bot(creds, config)
 
-asr = ASR(os.getenv('ASR_MODEL', 'tiny'), os.getenv('ASR_LANGUAGE', 'en'))
+asr = ASR(os.getenv("ASR_MODEL", "tiny"), os.getenv("ASR_LANGUAGE", "en"))
+
 
 @bot.listener.on_custom_event(nio.RoomMessage)
 async def on_message(room, event):
-  if not isinstance(event, (nio.RoomMessageAudio,
-                            nio.RoomEncryptedAudio,
-                            nio.RoomMessageVideo,
-                            nio.RoomEncryptedVideo)):
-    return
+    if not isinstance(
+        event,
+        (
+            nio.RoomMessageAudio,
+            nio.RoomEncryptedAudio,
+            nio.RoomMessageVideo,
+            nio.RoomEncryptedVideo,
+        ),
+    ):
+        return
 
-  encrypted = isinstance(event, (nio.RoomEncryptedAudio, nio.RoomEncryptedVideo))
+    encrypted = isinstance(event, (nio.RoomEncryptedAudio, nio.RoomEncryptedVideo))
 
-  print(room.machine_name, event.sender, event.body, event.url)
-  match = botlib.MessageMatch(room, event, bot)
-  if match.is_not_from_this_bot():
-    await bot.async_client.room_typing(room.machine_name, True, timeout=120000)
-    response = await bot.async_client.download(mxc=event.url)
-    if isinstance(response, nio.responses.DownloadError):
-      print(response)
-      data = None
-    elif encrypted:
-      print("decrypting...")
-      data = nio.crypto.attachments.decrypt_attachment(
-        response.body,
-        event.source["content"]["file"]["key"]["k"],
-        event.source["content"]["file"]["hashes"]["sha256"],
-        event.source["content"]["file"]["iv"],
-      )
-    else:
-      data = response.body
+    print(room.machine_name, event.sender, event.body, event.url)
+    match = botlib.MessageMatch(room, event, bot)
+    if match.is_not_from_this_bot():
+        await bot.async_client.room_typing(room.machine_name, True, timeout=120000)
+        response = await bot.async_client.download(mxc=event.url)
+        if isinstance(response, nio.responses.DownloadError):
+            print(response)
+            data = None
+        elif encrypted:
+            print("decrypting...")
+            data = nio.crypto.attachments.decrypt_attachment(
+                response.body,
+                event.source["content"]["file"]["key"]["k"],
+                event.source["content"]["file"]["hashes"]["sha256"],
+                event.source["content"]["file"]["iv"],
+            )
+        else:
+            data = response.body
 
-    filename = response.filename or event.body
-    if filename:
-        parsed_path = urlparse(filename).path
-        basename = os.path.basename(parsed_path)
-        _, ext = os.path.splitext(basename)
-        safe_suffix = ext if ext else ''
-    else:
-        safe_suffix = ''
+        filename = response.filename or event.body
+        if filename:
+            parsed_path = urlparse(filename).path
+            basename = os.path.basename(parsed_path)
+            _, ext = os.path.splitext(basename)
+            safe_suffix = ext if ext else ""
+        else:
+            safe_suffix = ""
 
-    if data:
-      with tempfile.NamedTemporaryFile("w+b", suffix=safe_suffix) as file:
-        file.write(data)
-        file.flush()
-        result = await asr.transcribe(file.name)
-    else:
-      result = None
-    
-    await bot.async_client.room_typing(room.machine_name, False)
+        if data:
+            with tempfile.NamedTemporaryFile("w+b", suffix=safe_suffix) as file:
+                file.write(data)
+                file.flush()
+                result = await asr.transcribe(file.name)
+        else:
+            result = None
 
-    if not result:
-      print("No result")
-      return
+        await bot.async_client.room_typing(room.machine_name, False)
 
-    if filename:
-      reply = f"Transcription of {filename}: {result}"
-    else:
-      reply = f"Transcription: {result}"
+        if not result:
+            print("No result")
+            return
 
-    await bot.api._send_room(
-      room_id=room.room_id,
-      content={
-        "msgtype": "m.notice",
-        "body": reply,
-        "m.relates_to": {
-          "m.in_reply_to": {
-            "event_id": event.event_id
-          }
-        }
-      })
+        if filename:
+            reply = f"Transcription of {filename}: {result}"
+        else:
+            reply = f"Transcription: {result}"
+
+        await bot.api._send_room(
+            room_id=room.room_id,
+            content={
+                "msgtype": "m.notice",
+                "body": reply,
+                "m.relates_to": {"m.in_reply_to": {"event_id": event.event_id}},
+            },
+        )
+
 
 async def main():
     asr.load_model()
@@ -118,6 +121,6 @@ async def main():
             print("Unexpected error, restarting...")
             await asyncio.sleep(5)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
--- speech_recognition.py
+++ speech_recognition.py
@@ -3,80 +3,86 @@ import subprocess
 import os
 
 MODELS = [
-  "tiny",
-  "tiny.en",
-  "tiny-q5_1",
-  "tiny.en-q5_1",
-  "tiny-q8_0",
-  "base",
-  "base.en",
-  "base-q5_1",
-  "base.en-q5_1",
-  "base-q8_0",
-  "small",
-  "small.en",
-  "small.en-tdrz",
-  "small-q5_1",
-  "small.en-q5_1",
-  "small-q8_0",
-  "medium",
-  "medium.en",
-  "medium-q5_0",
-  "medium.en-q5_0",
-  "medium-q8_0",
-  "large-v1",
-  "large-v2",
-  "large-v2-q5_0",
-  "large-v2-q8_0",
-  "large-v3",
-  "large-v3-q5_0",
-  "large-v3-turbo",
-  "large-v3-turbo-q5_0",
-  "large-v3-turbo-q8_0",
+    "tiny",
+    "tiny.en",
+    "tiny-q5_1",
+    "tiny.en-q5_1",
+    "tiny-q8_0",
+    "base",
+    "base.en",
+    "base-q5_1",
+    "base.en-q5_1",
+    "base-q8_0",
+    "small",
+    "small.en",
+    "small.en-tdrz",
+    "small-q5_1",
+    "small.en-q5_1",
+    "small-q8_0",
+    "medium",
+    "medium.en",
+    "medium-q5_0",
+    "medium.en-q5_0",
+    "medium-q8_0",
+    "large-v1",
+    "large-v2",
+    "large-v2-q5_0",
+    "large-v2-q8_0",
+    "large-v3",
+    "large-v3-q5_0",
+    "large-v3-turbo",
+    "large-v3-turbo-q5_0",
+    "large-v3-turbo-q8_0",
 ]
 
-class ASR():
-  def __init__(self, model = "tiny", language = "en"):
-    if os.path.exists(f"/app/ggml-{model}.bin"):
-      self.model_path = f"/app"
-    else:
-      self.model_path = f"/data/models"
-      if not os.path.exists(self.model_path):
-        os.mkdir(self.model_path)
 
-    file_path = f"{self.model_path}/ggml-{model}.bin"
-    if not os.path.exists(file_path) and model not in MODELS:
-      raise ValueError(f"Invalid model: {model}. Must be one of {MODELS}")
+class ASR:
+    def __init__(self, model="tiny", language="en"):
+        if os.path.exists(f"/app/ggml-{model}.bin"):
+            self.model_path = f"/app"
+        else:
+            self.model_path = f"/data/models"
+            if not os.path.exists(self.model_path):
+                os.mkdir(self.model_path)
 
-    self.model = model
-    self.language = language
-    self.file_path = file_path
-    self.lock = asyncio.Lock()
+        file_path = f"{self.model_path}/ggml-{model}.bin"
+        if not os.path.exists(file_path) and model not in MODELS:
+            raise ValueError(f"Invalid model: {model}. Must be one of {MODELS}")
 
-  def load_model(self):
-    if not os.path.exists(self.file_path) or os.path.getsize(self.file_path) == 0:
-      print("Downloading model...")
-      subprocess.run(["./download-ggml-model.sh", self.model, self.model_path], check=True)
-      print("Done.")
+        self.model = model
+        self.language = language
+        self.file_path = file_path
+        self.lock = asyncio.Lock()
 
-  async def transcribe(self, file: str) -> str:
-    async with self.lock:
-      print(f"Transcribing {file}...")
-      proc = await asyncio.create_subprocess_exec(
-          "./whisper-cli",
-          "-m", f"{self.model_path}/ggml-{self.model}.bin",
-          "-l", self.language,
-          "-f", file,
-          "-nt",
-          stdout=asyncio.subprocess.PIPE,
-          stderr=asyncio.subprocess.PIPE
-        )
-      stdout, stderr = await proc.communicate()
+    def load_model(self):
+        if not os.path.exists(self.file_path) or os.path.getsize(self.file_path) == 0:
+            print("Downloading model...")
+            subprocess.run(
+                ["./download-ggml-model.sh", self.model, self.model_path], check=True
+            )
+            print("Done.")
 
-    if stderr:
-      print(stderr.decode())
-      
-    text = stdout.decode().strip()
-    print(text)
+    async def transcribe(self, file: str) -> str:
+        async with self.lock:
+            print(f"Transcribing {file}...")
+            proc = await asyncio.create_subprocess_exec(
+                "./whisper-cli",
+                "-m",
+                f"{self.model_path}/ggml-{self.model}.bin",
+                "-l",
+                self.language,
+                "-f",
+                file,
+                "-nt",
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await proc.communicate()
 
-    return text
+        if stderr:
+            print(stderr.decode())
+
+        text = stdout.decode().strip()
+        print(text)
+
+        return text
